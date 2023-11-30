3297e7b98cfc08c52e41c6222cda2d0d27f732b9
Rhont <updownuper@gmail.com>
2023-11-30 16:47:51 +0300

    Нашел статью по периоду, сегодня все доделаю.

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,15 @@ import rule.TwoHourRuleImpl;
 import support.FlightBuilder;
 import support.FlightSelection;
 
+import java.time.LocalDateTime;
+import java.time.Period;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<Flight> flights = FlightBuilder.createFlights();
         flights.forEach(System.out::println);
+
         FlightSelection flightSelection=new FlightSelection();
         flightSelection.setFlights(flights);
 
@@ -18,6 +21,16 @@ public class Main {
                 getFlights();
 
         System.out.println(flights);
+
+        // вычитание даты, эффективность под сомнением
+        System.out.println();
+
+        LocalDateTime d1=LocalDateTime.of(2012,6,3,22,30);
+        LocalDateTime d2=LocalDateTime.of(2012,6,3,22,10);
+
+// https://sky.pro/media/raschyot-razniczy-mezhdu-dvumya-datami-v-java/
+
+        System.out.println(d1.getMinute()-d2.getMinute());
 
 
     }
