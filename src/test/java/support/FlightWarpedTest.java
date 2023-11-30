c50e121576c446417b3617c9ec14f4a9cf3fff53
RHONT <rg915i@yandex.ru>
2023-12-01 01:23:01 +0300

    Тесты - завтра...

---
+++ src/test/java/support/FlightWarpedTest.java
@@ -0,0 +1,23 @@
+package support;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FlightWarpedTest {
+
+
+
+    @Test
+    void setFlights() {
+    }
+
+    @Test
+    void setRules() {
+    }
+
+    @Test
+    void setSingleRule() {
+    }
+}
